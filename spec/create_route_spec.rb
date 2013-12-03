713331824fe5daf46846a07901ee4bce0e57110d
Jon Seidel <jseidel@edpci.com>
2013-12-03 15:14:27 -0800

    Cleaned up use of spec_helper...
    Moved it into spec/ directory and adjusted requires in specs

--- spec/create_route_spec.rb
+++ spec/create_route_spec.rb
@@ -1,4 +1,4 @@
-require '../spec_helper'
+require 'spec_helper'
 require 'route'
 
 describe Route do
--- spec/input_digraph_spec.rb
+++ spec/input_digraph_spec.rb
@@ -1,4 +1,4 @@
-require '../spec_helper'
+require 'spec_helper'
 require 'digraph'
 
 describe 'Digraph input' do
